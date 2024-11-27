5386ed33bd6c6fb8899cc8a38a2d66eaaa1d5d8c
Carlos Barrios <carlosbarriosf6@gmail.com>
2024-11-27 14:26:36 -0300

    added a searchBar to the discover lists page and modified the api route to be able to use it

--- app/discover-lists/page.jsx
+++ app/discover-lists/page.jsx
@@ -9,6 +9,8 @@ const DiscoverLists = () => {
   const [lists, setLists] = useState([])
   const { data: session} = useSession();
 
+  const [searchValue, setSearchValue] = useState('')
+
   useEffect(() => {
     const getLists = async () => {
       const res = await fetch('/api/list');
@@ -20,7 +22,29 @@ const DiscoverLists = () => {
   }, [])
 
   return (
-    <div>
+    <section>
+      <h1 className='text-center text-base sm:text-xl font-semibold text-indigo-500 my-4'>Find new lists to play!</h1>
+      <div className='flex gap-4 justify-center my-4 px-2'>
+        <div className="searchBar">
+          <input 
+            className='px-2 py-1 text-sm rounded-md border-indigo-400 border' 
+            type="text" 
+            placeholder='Search by title...'
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+              
+            }}
+          />
+        </div>
+        <div className="filter text-sm">
+          <select name="" id="" className='px-2 py-1 rounded-md border-indigo-400 border'>
+            <option selected disabled value="Filter by">Filter by</option>
+            <option value="Likes">Likes</option>
+            <option value="Title">Title</option>
+          </select>
+        </div>
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-2'>
             {lists.length > 0 && 
               lists.map(entry => (
@@ -33,7 +57,7 @@ const DiscoverLists = () => {
               ))
             }
           </div>
-    </div>
+    </section>
   )
 }
 
