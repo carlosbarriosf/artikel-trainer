c00301a07622ccbae028c86dfda14f45d09f07a6
Carlos Barrios <carlosbarriosf6@gmail.com>
2024-12-28 12:40:56 -0300

    added footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,35 @@
+import Link from 'next/link';
 import React from 'react'
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <footer className='w-full border border-red-600 mt-4'>
-        footer
+    <footer className='w-full py-4 flex flex-col gap-1'>
+      <div className='flex justify-between'>
+        <div className='flex gap-2'>
+          <Link href='https://www.linkedin.com/in/carlosbarriosf/'>
+            <FaLinkedin size={16}/>
+          </Link>
+          <Link href='https://github.com/carlosbarriosf'>
+            <FaGithub size={16}/>
+          </Link>
+        </div>
+        <div>
+          Logo
+        </div>
+      </div>
+      <div className='flex items-center text-xs gap-2'>
+        <Link href='mailto:carlosbarriosdev@gmail.com' target='_blank'>
+          <IoMdMail size={16}/>
+        </Link>
+        <p>Send feedback or report an issue</p>
+      </div>
+      <div  className='text-center text-xs'>
+      <p>Built with Next.js, Mongoose, and love for the German language.</p>
+      <p>© 2024 Carlos Barrios. All rights reserved.</p>
+      </div>
     </footer>
   )
 }
