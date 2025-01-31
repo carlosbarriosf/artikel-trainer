d95181e7099c20da6c714310e39212a5be32b87c
Carlos Barrios <carlosbarriosf6@gmail.com>
2025-01-31 16:17:15 -0300

    added error handling in api call, and fixed incorrect variable naming in create/edit list page

--- app/create-list/page.jsx
+++ app/create-list/page.jsx
@@ -1,99 +1,106 @@
 "use client";
 
-import List from '@components/List';
-import { useSession } from 'next-auth/react';
-import React, { useState } from 'react'
-
+import List from "@components/List";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
 
 const LIST_INITIAL_STATE = {
-  name : '',
-  words: []
-}
+  name: "",
+  words: [],
+};
 
 const CreateList = () => {
+  const { data: session } = useSession();
 
-  const {data: session} = useSession()
-
-
-  const [list, setList] = useState(LIST_INITIAL_STATE)
+  const [list, setList] = useState(LIST_INITIAL_STATE);
   const [values, setValues] = useState({
-    noun: '',
-    article: '',
-    plural: ''
-  })
+    noun: "",
+    article: "",
+    plural: "",
+  });
   const [formStatus, setFormStatus] = useState({
-    noun: '',
-    article: '',
-    plural: true
-  })
-  const [message, setMessage] = useState('')
-  const [messageClassName, setMessageClassName] = useState('')
+    noun: "",
+    article: "",
+    plural: true,
+  });
+  const [message, setMessage] = useState("");
+  const [messageClassName, setMessageClassName] = useState("");
 
-  const [submittingForm, setSubmittingForm] = useState(false)
+  const [savingList, setSavingList] = useState(false);
 
   //handlers
 
   const handleDeleteNounBtn = (item) => {
-    const filteredWords = list.words.filter(word => word.noun !== item.noun)
-      console.log(item.noun)
-      console.log(filteredWords)
-      setList(prevList => ({
-        ...prevList,
-        words: filteredWords
-      }))
-  }
+    const filteredWords = list.words.filter((word) => word.noun !== item.noun);
+    console.log(item.noun);
+    console.log(filteredWords);
+    setList((prevList) => ({
+      ...prevList,
+      words: filteredWords,
+    }));
+  };
 
   const handleMessage = (msg, bgColor) => {
     setMessage(msg);
     setMessageClassName(`${bgColor} appear`);
     setTimeout(() => {
-      setMessageClassName(`${bgColor} disappear`)
+      setMessageClassName(`${bgColor} disappear`);
     }, 2700);
     setTimeout(() => {
-      setMessage('')
+      setMessage("");
     }, 3000);
-  }
+  };
 
   const handleSaveList = async () => {
-    if (list.name === '') {
-      handleMessage('The list must have a name', 'bg-amber-500')
-      return
-    } 
-    if (list.words.length < 20 || list.words.length > 40) {
-      handleMessage('The list must contain between 20 and 40 nouns', 'bg-amber-500')
-      return
+    if (list.name === "") {
+      handleMessage("The list must have a name", "bg-amber-500");
+      return;
     }
-    
-    setSubmittingForm(true)
+    if (list.words.length < 20 || list.words.length > 40) {
+      handleMessage(
+        "The list must contain between 20 and 40 nouns",
+        "bg-amber-500"
+      );
+      return;
+    }
+
+    setSavingList(true);
 
     try {
-      const res = await fetch('/api/list/new', {
-        method: 'POST',
+      const res = await fetch("/api/list/new", {
+        method: "POST",
         body: JSON.stringify({
           userId: session?.user.id,
-          list
-        })
-      })
-      const data = await res.json()
-      console.log(data)
+          list,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
       if (res.status === 201) {
-        handleMessage(data.message, 'bg-green-600')
-        setList(LIST_INITIAL_STATE)
+        handleMessage(data.message, "bg-green-600");
+        setList(LIST_INITIAL_STATE);
       } else if (res.status === 403) {
-        handleMessage(data.message, 'bg-amber-500')
+        handleMessage(data.message, "bg-amber-500");
+      } else if (res.status === 500) {
+        handleMessage(data.message, "bg-red-500");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      handleMessage("Something went wrong...", "bg-red-500");
     } finally {
-      setSubmittingForm(false)
+      setSavingList(false);
     }
-    }
-  
+  };
+
   return (
     <section>
-      <h1 className='text-center text-cyan-800 text-xl sm:text-2xl mb-4 font-bold'>Create a new list of nouns!</h1>
-      <h2 className='text-center mb-4 font-semibold text-indigo-500'>{list?.name || 'Your list name'}</h2>
-      <List 
+      <h1 className="text-center text-cyan-800 text-xl sm:text-2xl mb-4 font-bold">
+        Create a new list of nouns!
+      </h1>
+      <h2 className="text-center mb-4 font-semibold text-indigo-500">
+        {list?.name || "Your list name"}
+      </h2>
+      <List
         list={list}
         handleDeleteNounBtn={handleDeleteNounBtn}
         setList={setList}
@@ -105,10 +112,10 @@ const CreateList = () => {
         handleSaveList={handleSaveList}
         formStatus={formStatus}
         setFormStatus={setFormStatus}
-        submittingForm={submittingForm}
+        savingList={savingList}
       />
     </section>
-  )
-}
+  );
+};
 
-export default CreateList
+export default CreateList;
--- app/edit-list/page.jsx
+++ app/edit-list/page.jsx
@@ -1,156 +1,163 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import List from '@components/List'
-import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Suspense, useEffect, useState } from 'react'
+import List from "@components/List";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const EditList = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const listId = searchParams.get("id");
+  const [fetchingList, setFetchingList] = useState(true);
 
-    const searchParams = useSearchParams();
-    const router = useRouter()
-    const listId = searchParams.get('id')
-    const [fetchingList, setFetchingList] = useState(true)
+  useEffect(() => {
+    try {
+      const getList = async () => {
+        const res = await fetch(`/api/list/${listId}`);
+        const data = await res.json();
+        console.log(data.list);
+        setList(data.list);
+      };
+      getList();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setFetchingList(false);
+    }
+  }, []);
 
-    useEffect(() => {
-      try {
-          const getList = async () => {
-              const res = await fetch(`/api/list/${listId}`)
-              const data = await res.json()
-              console.log(data.list)
-              setList(data.list)
-          }
-          getList()
-        } catch (error) {
-          console.error(error)
-        } finally {
-          setFetchingList(false)
-        }
-    }, [])
-    
-    
-    const [list, setList] = useState()
-    const [values, setValues] = useState({
-        noun: '',
-        article: '',
-        plural: ''
-    })
-    const [formStatus, setFormStatus] = useState({
-        noun: '',
-        article: '',
-        plural: true
-    })
-    const [message, setMessage] = useState('')
-    const [messageClassName, setMessageClassName] = useState('')
+  const [list, setList] = useState();
+  const [values, setValues] = useState({
+    noun: "",
+    article: "",
+    plural: "",
+  });
+  const [formStatus, setFormStatus] = useState({
+    noun: "",
+    article: "",
+    plural: true,
+  });
+  const [message, setMessage] = useState("");
+  const [messageClassName, setMessageClassName] = useState("");
 
-    const [submittingForm, setSubmittingForm] = useState(false)
+  const [savingList, setSavingList] = useState(false);
 
-    //handlers
+  //handlers
 
-    const handleDeleteNounBtn = (item) => {
-        const filteredWords = list.words.filter(word => word.noun !== item.noun)
-        console.log(item.noun)
-        console.log(filteredWords)
-        setList(prevList => ({
-            ...prevList,
-            words: filteredWords
-        }))
+  const handleDeleteNounBtn = (item) => {
+    const filteredWords = list.words.filter((word) => word.noun !== item.noun);
+    console.log(item.noun);
+    console.log(filteredWords);
+    setList((prevList) => ({
+      ...prevList,
+      words: filteredWords,
+    }));
+  };
+
+  const handleMessage = (msg, bgColor) => {
+    setMessage(msg);
+    setMessageClassName(`${bgColor} appear`);
+    setTimeout(() => {
+      setMessageClassName(`${bgColor} disappear`);
+    }, 2700);
+    setTimeout(() => {
+      setMessage("");
+    }, 3000);
+  };
+
+  const handleSaveList = async () => {
+    if (list.name === "") {
+      handleMessage("The list must have a name", "bg-amber-500");
+      return;
+    }
+    if (list.words.length < 20 || list.words.length > 40) {
+      handleMessage(
+        "The list must contain between 20 and 40 nouns",
+        "bg-amber-500"
+      );
+      return;
     }
 
-    const handleMessage = (msg, bgColor) => {
-        setMessage(msg);
-        setMessageClassName(`${bgColor} appear`);
+    setSavingList(true);
+    try {
+      const res = await fetch(`/api/list/${listId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          list: list,
+        }),
+      });
+      if (res.ok) {
+        handleMessage("List updated!", "bg-green-600");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSavingList(false);
+    }
+  };
+
+  const handleDeleteList = async () => {
+    const confirmation = confirm(
+      "This action is irreversible. Are you sure you want to procceed?"
+    );
+    if (!confirmation) return;
+
+    setSavingList(true);
+
+    try {
+      const res = await fetch(`/api/list/${listId}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        handleMessage("List deleted successfully", "bg-green-600");
         setTimeout(() => {
-        setMessageClassName(`${bgColor} disappear`)
-        }, 2700);
-        setTimeout(() => {
-        setMessage('')
-        }, 3000);
-    }
-
-    const handleSaveList = async () => {
-      if (list.name === '') {
-        handleMessage('The list must have a name', 'bg-amber-500')
-        return
-      } 
-      if (list.words.length < 20 || list.words.length > 40) {
-        handleMessage('The list must contain between 20 and 40 nouns', 'bg-amber-500')
-        return
+          router.push("/");
+        }, 1200);
       }
-
-      setSubmittingForm(true)
-      try {
-        const res = await fetch(`/api/list/${listId}`, {
-          method: 'PATCH',
-          body: JSON.stringify({
-            list: list
-          })
-        })
-        if(res.ok) {
-          handleMessage('List updated!', 'bg-green-600')
-        }
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setSubmittingForm(false)
-      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSavingList(false);
     }
-
-    const handleDeleteList = async () => {
-      const confirmation = confirm('This action is irreversible. Are you sure you want to procceed?')
-      if(!confirmation) return
-
-      setSubmittingForm(true)
-
-      try {
-        const res = await fetch(`/api/list/${listId}`, {
-          method: 'DELETE'
-        })
-        if(res.ok) {
-          handleMessage('List deleted successfully', 'bg-green-600')
-          setTimeout(() => {
-            router.push('/')
-          }, 1200);
-        }
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setSubmittingForm(false)
-      }
-    }
+  };
 
   return (
-      <section>
-        { fetchingList ?
-          <div className='bigSpinner'></div>
-          :
-          <>
-            <h1 className='text-center text-cyan-800 text-xl sm:text-2xl mb-4 font-bold'>Edit your list!</h1>
-            {list &&
-              <h2 className='text-center mb-4 font-semibold text-indigo-500'>{list.name}</h2>
-            }  
-            {list && 
-              <List 
-                list={list}
-                handleDeleteNounBtn={handleDeleteNounBtn}
-                setList={setList}
-                setValues={setValues}
-                values={values}
-                message={message}
-                messageClassName={messageClassName}
-                handleMessage={handleMessage}
-                handleSaveList={handleSaveList}
-                formStatus={formStatus}
-                setFormStatus={setFormStatus}
-                edit={true}
-                handleDeleteList={handleDeleteList}
-                submittingForm={submittingForm}
-              />
-            }
-          </>
-        }
-      </section>
-  )
-}
+    <section>
+      {fetchingList ? (
+        <div className="bigSpinner"></div>
+      ) : (
+        <>
+          <h1 className="text-center text-cyan-800 text-xl sm:text-2xl mb-4 font-bold">
+            Edit your list!
+          </h1>
+          {list && (
+            <h2 className="text-center mb-4 font-semibold text-indigo-500">
+              {list.name}
+            </h2>
+          )}
+          {list && (
+            <List
+              list={list}
+              handleDeleteNounBtn={handleDeleteNounBtn}
+              setList={setList}
+              setValues={setValues}
+              values={values}
+              message={message}
+              messageClassName={messageClassName}
+              handleMessage={handleMessage}
+              handleSaveList={handleSaveList}
+              formStatus={formStatus}
+              setFormStatus={setFormStatus}
+              edit={true}
+              handleDeleteList={handleDeleteList}
+              savingList={savingList}
+            />
+          )}
+        </>
+      )}
+    </section>
+  );
+};
 
-export default EditList
+export default EditList;
